f8e1b16344432ecd9f71aef2441aa538680a6615
anjara <loranasl17@gmail.com>
2026-05-13 11:17:16 +0300

    fichiers init middleware

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -3,7 +3,39 @@
 namespace App\Controllers;
 
 class AuthController extends BaseController {
-    public function login() {
+    public function form()
+    {
         return view('auth/login');
+    }
+
+    // public function login() {
+    //     return view('auth/login');
+    // }
+
+    public function login()
+    {
+        $model = new UserModel();
+        $email = $this->request->getPost('email');
+        $password = $this->request->getPost('password');
+        $user = $model->where('email', $email)->first();
+        if (!$user || !password_verify($password, $user['password'])) {
+            return view('auth/login', [
+            'erreur' => 'Email ou mot de passe incorrect'
+            ]);
+        }
+        // Stocker uniquement les données non sensibles en session
+        session()->set('user', [
+            'id' => $user['id'],
+            'nom' => $user['nom'],
+            'email' => $user['email'],
+            'role' => $user['role'],
+        ]);
+        return redirect()->to('/livres');
+    }
+
+    public function logout()
+    {
+        session()->destroy();
+        return redirect()->to('/login');
     }
 }
